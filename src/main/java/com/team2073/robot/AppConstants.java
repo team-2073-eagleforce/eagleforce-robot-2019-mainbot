5c62f95e60ba01c761b4b08b4a15eb1753edfc75
Jason2073 <stanleytechrepair@gmail.com>
2018-11-23 22:02:26 -0800

    Outlined ExampleArmSubsystem

--- src/main/java/com/team2073/robot/AppConstants.java
+++ src/main/java/com/team2073/robot/AppConstants.java
@@ -1,4 +1,18 @@
 package com.team2073.robot;
 
-public class AppConstants {
+public abstract class AppConstants {
+	public abstract class Ports {
+		public static final int EXAMPLE_ARM_SUBSYSTEM_TALON = 0;
+		public static final int EXAMPLE_ARM_SUBSYSTEM_VICTOR = 0;
+		public static final int EXAMPLE_ARM_HALL_EFFECT_SENSOR = 0;
+
+		public static final int DRIVE_LEFT_TALON = 1;
+		public static final int DRIVE_RIGHT_TALON = 2;
+		public static final int DRIVE_LEFT_VICTOR = 1;
+		public static final int DRIVE_RIGHT_VICTOR = 2;
+
+		public static final int PIGEON_GYRO = 0;
+	}
+
+
 }
