b0eb39cce59f919dc838393432b0806f20186e54
TheMinja <retrominh@gmail.com>
2019-01-10 16:42:07 -0800

    DS-13: Moved Robot Methods to RobotDelegate

--- src/main/java/com/team2073/robot/RobotDelegate.java
+++ src/main/java/com/team2073/robot/RobotDelegate.java
@@ -1,7 +1,28 @@
 package com.team2073.robot;
 
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.mycila.guice.ext.closeable.CloseableModule;
+import com.mycila.guice.ext.jsr250.Jsr250Module;
+import com.team2073.common.periodic.PeriodicRunner;
 import com.team2073.common.robot.AbstractRobotDelegate;
+import com.team2073.robot.ctx.RobotMapModule;
 
 public class RobotDelegate extends AbstractRobotDelegate {
 
+    @Inject
+    private PeriodicRunner periodicRunner;
+
+    @Override
+    public void robotInit() {
+        Injector injector = Guice.createInjector(new RobotMapModule(), new CloseableModule(), new Jsr250Module());
+        injector.getInstance(OperatorInterface.class);
+        injector.injectMembers(this);
+    }
+
+    @Override
+    public void robotPeriodic() {
+
+    }
 }
