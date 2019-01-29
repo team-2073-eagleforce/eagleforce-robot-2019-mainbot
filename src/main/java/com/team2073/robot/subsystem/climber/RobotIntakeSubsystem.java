7ca9e5e3b0632b4542dd23f24057cd4e311862a2
Jimmy Nguyen <j99717690@gmail.com>
2019-01-28 20:48:14 -0800

    DS-23: added switch statement which checks for the different states for the forks and clamp

--- src/main/java/com/team2073/robot/subsystem/climber/RobotIntakeSubsystem.java
+++ src/main/java/com/team2073/robot/subsystem/climber/RobotIntakeSubsystem.java
@@ -2,6 +2,8 @@ package com.team2073.robot.subsystem.climber;
 
 import com.team2073.common.ctx.RobotContext;
 import com.team2073.common.periodic.PeriodicRunnable;
+import com.team2073.common.util.ExceptionUtil;
+import com.team2073.common.util.Timer;
 import com.team2073.robot.ctx.ApplicationContext;
 import com.team2073.robot.mediator.StateSubsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -9,37 +11,60 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import static com.team2073.robot.subsystem.climber.RobotIntakeSubsystem.RobotIntakeState;
 
 public class RobotIntakeSubsystem implements PeriodicRunnable, StateSubsystem<RobotIntakeState> {
-	private final RobotContext robotCtx = RobotContext.getInstance();
-	private final ApplicationContext appCtx = ApplicationContext.getInstance();
+    private final RobotContext robotCtx = RobotContext.getInstance();
+    private final ApplicationContext appCtx = ApplicationContext.getInstance();
 
-	private DoubleSolenoid forkSolenoid = appCtx.getForkDeploySolenoid();
-	private DoubleSolenoid robotGrabSolenoid = appCtx.getRobotGrabSolenoid();
+    private Timer timer = new Timer();
+    private DoubleSolenoid forkSolenoid = appCtx.getForkDeploySolenoid();
+    private DoubleSolenoid robotGrabSolenoid = appCtx.getRobotGrabSolenoid();
 
-	private RobotIntakeState state = RobotIntakeState.STORE;
+    private RobotIntakeState state = RobotIntakeState.STORE;
 
-	public RobotIntakeSubsystem() {
-		autoRegisterWithPeriodicRunner();
-	}
+    public RobotIntakeSubsystem() {
+        autoRegisterWithPeriodicRunner();
+    }
 
-	@Override
-	public RobotIntakeState currentState() {
-		return state;
-	}
+    @Override
+    public RobotIntakeState currentState() {
+        return state;
+    }
 
-	@Override
-	public void set(RobotIntakeState goalState) {
-		state = goalState;
-	}
+    @Override
+    public void set(RobotIntakeState goalState) {
+        state = goalState;
+    }
 
-	@Override
-	public void onPeriodic() {
+    @Override
+    public void onPeriodic() {
+        switch(state){
+            case STORE:
+                forkSolenoid.set(DoubleSolenoid.Value.kReverse);
+                robotGrabSolenoid.set(DoubleSolenoid.Value.kReverse);
+                break;
+            case DEPLOY_FORKS:
+                forkSolenoid.set(DoubleSolenoid.Value.kForward);
+                timer.start();
+                if(timer.getElapsedTime() > 1){
+                    robotGrabSolenoid.set(DoubleSolenoid.Value.kReverse);
+                }
+                break;
+            case OPEN_INTAKE:
+                robotGrabSolenoid.set(DoubleSolenoid.Value.kReverse);
+                break;
+            case CLAMP:
+                robotGrabSolenoid.set(DoubleSolenoid.Value.kForward);
+                break;
+            default:
+                throw new IllegalStateException();
+        }
 
-	}
+    }
 
-	public enum RobotIntakeState {
-		STORE,
-		DEPLOY_FORKS,
-		OPEN_INTAKE,
-		CLAMP;
-	}
-}
+    public enum RobotIntakeState {
+        STORE,
+        DEPLOY_FORKS,
+        OPEN_INTAKE,
+        CLAMP
+        }
+    }
+
