9ebf4e39829c10b3c785babce172f694d3b4e978
matthew <you@example.com>
2019-02-04 17:18:24 -0800

    DS-35: added a command class for each of my states.

---
+++ src/main/java/com/team2073/robot/command/intakeRoller/IntakeCommand.java
@@ -0,0 +1,21 @@
+package com.team2073.robot.command.intakeRoller;
+
+import com.team2073.common.command.AbstractLoggingCommand;
+import com.team2073.robot.ctx.ApplicationContext;
+import com.team2073.robot.mediator.Mediator;
+import com.team2073.robot.subsystem.intake.IntakeRollerSubsystem;
+
+public class IntakeCommand extends AbstractLoggingCommand {
+    private ApplicationContext appCtx = ApplicationContext.getInstance();
+    private Mediator mediator = appCtx.getMediator();
+
+    @Override
+    protected void initializeDelegate() {
+        mediator.intakeRollers(IntakeRollerSubsystem.IntakeRollerState.INTAKE_SPEED);
+    }
+
+    @Override
+    protected boolean isFinishedDelegate() {
+        return true;
+    }
+}
---
+++ src/main/java/com/team2073/robot/command/intakeRoller/IntakeStopCommand.java
@@ -0,0 +1,23 @@
+package com.team2073.robot.command.intakeRoller;
+
+import com.team2073.common.command.AbstractLoggingCommand;
+import com.team2073.robot.ctx.ApplicationContext;
+import com.team2073.robot.mediator.Mediator;
+import com.team2073.robot.subsystem.intake.IntakeRollerSubsystem;
+
+public class IntakeStopCommand extends AbstractLoggingCommand {
+
+
+    private ApplicationContext appCtx = ApplicationContext.getInstance();
+    private Mediator mediator = appCtx.getMediator();
+
+    @Override
+    protected void initializeDelegate() {
+        mediator.intakeRollers(IntakeRollerSubsystem.IntakeRollerState.STOP);
+    }
+
+    @Override
+    protected boolean isFinishedDelegate() {
+        return true;
+    }
+}
