3d90867c7992b06c8e2e9ef18a3877152e37073d
Jason2073 <stanleytechrepair@gmail.com>
2019-02-24 11:00:22 -0800

    Converted to new wpilib version.

--- src/main/java/com/team2073/robot/command/IntakePivotCommand.java
+++ src/main/java/com/team2073/robot/command/IntakePivotCommand.java
@@ -3,7 +3,6 @@ package com.team2073.robot.command;
 import com.team2073.common.command.AbstractLoggingCommand;
 import com.team2073.robot.AppConstants;
 import com.team2073.robot.ctx.ApplicationContext;
-import sun.awt.AppContext;
 
 public class IntakePivotCommand extends AbstractLoggingCommand {
     private ApplicationContext appctx = ApplicationContext.getInstance();
